68605844eb426743c057879b66cd0b99ff80cb9a
swikar-napit <napit.swikar1@gmail.com>
2026-06-05 22:35:28 +0545

    Assignment 1

--- Day-1_Assignment/Corejsfeatures.js
+++ Day-1_Assignment/Corejsfeatures.js
@@ -42,3 +42,46 @@ let user = {
 const { name, country } = user
 console.log(name)
 console.log(country)
+
+//6 Array Destructuring
+let numbers = [10, 20, 30, 40]
+
+let [first, second] = numbers
+console.log(first)
+console.log(second)
+
+//7 Default Parameters
+function sayHello(name = "Guest") {
+  console.log(`Hello, ${name}!`)
+}
+
+sayHello()
+sayHello("Pujan")
+
+//8 Rest Operator (Sum of Numbers)
+function sumAll(...numbers) {
+  return numbers.reduce((total, num) => total + num, 0)
+}
+
+console.log(sumAll(1, 2, 3, 4))
+
+//9 Spread Operator with Arrays
+let arr1 = [1, 2, 3]
+let arr2 = [4, 5]
+
+let mergedArray = [...arr1, ...arr2]
+console.log(mergedArray)
+
+//10 Merge and Destructure Objects
+let obj1 = { a: 1, b: 2 }
+let obj2 = { c: 3, d: 4 }
+
+// Merge using spread operator
+const mergedObj = { ...obj1, ...obj2 }
+
+// Destructure from the merged object
+const { a, d } = mergedObj
+
+console.log("Merged Object:", mergedObj)
+console.log("a:", a)             
+console.log("d:", d)                 
